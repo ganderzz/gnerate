54a2f4218f253b6589f54f4aa682de128b4e37af
ganderzz <stolen3@gmail.com>
2017-09-06 18:44:09 -0700

    Fix comments

--- src/Classes/Gnerate.ts
+++ src/Classes/Gnerate.ts
@@ -2,13 +2,13 @@ import Utilities from "./Utilities";
 
 export default class Gnerate {
     public static showManPage() {
-        console.log("==========  rgen  ==========");
-        console.log("rgen [templateName] [path/name]  -  Generate a new file from a template to a path relative to the cwd.");
+        console.log("==========  gnerate  ==========");
+        console.log("gnerate [templateName] [path/name]  -  Generate a new file from a template to a path relative to the cwd.");
     }
 
     public static run(argv: string[]) {
         if(argv.length <= 1) {
-            console.log("rgen requires two arguments.");
+            console.log("\ngnerate requires two arguments.");
             Gnerate.showManPage();
         
             return;
